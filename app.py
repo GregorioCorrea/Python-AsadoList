d39170f92ed089eb6e96e884d7bb3725f68f1a0e
GregorioCorrea <110120893+GregorioCorrea@users.noreply.github.com>
2024-05-23 15:44:30 -0300

    Resuelto el problema de las PartitionKey

--- app.py
+++ app.py
@@ -12,15 +12,12 @@ table_service = TableService(account_name=account_name, account_key=account_key)
 
 # Crear la tabla si no existe
 table_name = 'ShoppingList'
-#if not table_service.exists(table_name):
-#    table_service.create_table(table_name)
-
-# Lista de compras en memoria
-#shopping_list = []
+if not table_service.exists(table_name):
+    table_service.create_table(table_name)
 
 @app.route('/')
 def home():
-    items = table_service.query_entities(table_name)
+    items = table_service.query_entities(table_name, filter="PartitionKey eq 'shopping'")
     return render_template('index.html', items=items)
 
 @app.route('/add_item', methods=['POST'])
@@ -28,7 +25,6 @@ def add_item():
     item = request.form.get('item')
     quantity = request.form.get('quantity')
     if item and quantity:
- #       shopping_list.append({'item': item, 'quantity': quantity})
         new_item = Entity()
         new_item.PartitionKey = 'shopping'
         new_item.RowKey = item
@@ -38,21 +34,8 @@ def add_item():
 
 @app.route('/get_list', methods=['GET'])
 def get_list():
-#    return jsonify(shopping_list)
-    items = table_service.query_entities(table_name)
+    items = table_service.query_entities(table_name, filter="PartitionKey eq 'shopping'")
     return jsonify([{'item': item.RowKey, 'quantity': item.quantity} for item in items])
-
-@app.route('/test_connection')
-def test_connection():
-    try:
-        test_entity = Entity()
-        test_entity.PartitionKey = 'test'
-        test_entity.RowKey = 'test'
-        test_entity.test_property = 'test_value'
-        table_service.insert_or_replace_entity(table_name, test_entity)
-        return 'Connection to Azure Table Storage is successful.'
-    except Exception as e:
-        return f'Error: {str(e)}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
